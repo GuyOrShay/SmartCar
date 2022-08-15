51658578b59f6163b3e379a0cee6103637abaa42
GuyOrShay <guyorshay2406@gmail.com>
2022-08-15 20:09:51 +0300

    Start Implement Basic Functions

---
+++ Readme.md
@@ -0,0 +1,5 @@
+ # Smart kay - Raspbarry Pi
+ ## The full drop box data is here : 
+ https://www.dropbox.com/sh/e4tb0g94na33tyx/AADORIBUyGxFvDU0WhKz9eJ-a?dl=0
+
+ ## This drop box contains examples and docs.
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
+from Constants.GpioAddresses import ULTRASONIC_GPIO_ECHO, ULTRASONIC_GPIO_TRIGGER
 import RPi.GPIO as GPIO
 import time
  
 GPIO.setmode(GPIO.BCM)
   
-#define GPIO pin
-GPIO_TRIGGER = 14
-GPIO_ECHO = 4
+#Import GPIO pin
 
 #set GPIO mode (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN )
+GPIO.setup(ULTRASONIC_GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(ULTRASONIC_GPIO_ECHO, GPIO.IN )
 
 def distance():
     # 10us is the trigger signal
-    GPIO.output(GPIO_TRIGGER, GPIO.HIGH)
+    GPIO.output(ULTRASONIC_GPIO_TRIGGER, GPIO.HIGH)
     time.sleep(0.00001)  #10us
-    GPIO.output(GPIO_TRIGGER, GPIO.LOW)
-    # while not GPIO.input(GPIO_ECHO):
+    GPIO.output(ULTRASONIC_GPIO_TRIGGER, GPIO.LOW)
+    # while not GPIO.input(ULTRASONIC_GPIO_ECHO):
     #     pass
     
     t1 = time.time()
-    while GPIO.input(GPIO_ECHO):
+    while GPIO.input(ULTRASONIC_GPIO_ECHO):
         pass
     t2 = time.time()
     print("distance is %d " % (((t2 - t1)* 340 / 2) * 100))
